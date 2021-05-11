7958ed134acaa1f860739e992a46d5442ed73270
Gianluigi Spisto <luigi.spisto92@gmail.com>
2021-05-11 10:52:24 +0200

    Aggiunto menu active

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -5,9 +5,9 @@
     {{-- nav bar (home,prodotti,news) --}}
     <nav class="nav_top mt-30">
         <ul>
-            <li><a href="{{route('homepage')}}" class="active">Home</a></li>
-            <li><a href="">Prodotti</a></li>
-            <li><a href="{{route('news')}}">News</a></li>
+            <li class="{{Route::getCurrentRoute()->getName() == 'homepage' ? 'active' : ''}}"><a href="{{route('homepage')}}">Home</a></li>
+            <li class="{{Route::getCurrentRoute()->getName() == 'product' ? 'active' : ''}}"><a href="{{route('product', ['id' => 0])}}">Prodotti</a></li>
+            <li class="{{Route::getCurrentRoute()->getName() == 'news' ? 'active' : ''}}"><a href="{{route('news')}}">News</a></li>
         </ul>
     </nav>
 </header>
